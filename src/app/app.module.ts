c939d118121bcabe3e0cbfa732dd3f0ca02d2ff3
DenZ <https://github.com/DZz1358/Jkaros.git>
2022-02-06 20:41:20 +0200

    create routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    MainLayoutComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
